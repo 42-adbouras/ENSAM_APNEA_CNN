1f3551c0e6f137e0c7f22b4bfbdaae3b7ee1fc56
42-adbouras <wikaz077@gmail.com>
2026-09-25 12:08:23 +0000

    refact: model structure to one notebook file model.ipynb update: added comments for src/model.ipynb

--- src/download.py
+++
@@ -1,8 +0,0 @@
-import wfdb
-from utils import DATA_DIR
-
-SAVE_DIR = "apnea-ecg"
-DATA_DIR.mkdir(parents=True, exist_ok=True)
-
-print(f"Downloading '{SAVE_DIR}' -> {DATA_DIR}")
-wfdb.dl_database(SAVE_DIR, dl_dir=str(DATA_DIR), overwrite=False)
